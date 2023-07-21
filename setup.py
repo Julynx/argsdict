dece954f57127971fdd42092cb73c8ce6715bd5d
Julio <julicabria@outlook.com>
2023-07-21 15:08:44 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="argsdict",
-    version="0.0.1",
+    version="1.0.0",
     author="Julio Cabria",
     author_email="juliocabria@tutanota.com",
     maintainer="Julio Cabria",
